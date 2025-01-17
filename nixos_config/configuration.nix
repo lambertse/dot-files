9d702ec7d51c577c0ebb9cd863e5ea4a60eb87a0
lambertse <lambert.software.engineer@gmail.com>
2025-01-17 11:53:30 +0700

    Hyprlock | Custimize lockscreen using hyprlock

--- nixos_config/configuration.nix
+++ nixos_config/configuration.nix
@@ -1,110 +1,114 @@
-# iEdit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  # Imports
+  imports = [
+    ./hardware-configuration.nix  # Include the results of the hardware scan.
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Nix settings
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Ho_Chi_Minh";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "vi_VN";
-    LC_IDENTIFICATION = "vi_VN";
-    LC_MEASUREMENT = "vi_VN";
-    LC_MONETARY = "vi_VN";
-    LC_NAME = "vi_VN";
-    LC_NUMERIC = "vi_VN";
-    LC_PAPER = "vi_VN";
-    LC_TELEPHONE = "vi_VN";
-    LC_TIME = "vi_VN";
+  # Bootloader
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
 
-  # Configure keymap in X11
+  # Networking
+  networking = {
+    hostName = "nixos";  # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # proxy.default = "http://user:password@proxy:port/";  # Configure network proxy if necessary.
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";  # Proxy exceptions.
+
+    networkmanager.enable = true;  # Enable networking
+  };
+
+  # Hardware
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  # Time zone
+  time.timeZone = "Asia/Ho_Chi_Minh";
+
+  # Internationalisation
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "vi_VN";
+      LC_IDENTIFICATION = "vi_VN";
+      LC_MEASUREMENT = "vi_VN";
+      LC_MONETARY = "vi_VN";
+      LC_NAME = "vi_VN";
+      LC_NUMERIC = "vi_VN";
+      LC_PAPER = "vi_VN";
+      LC_TELEPHONE = "vi_VN";
+      LC_TIME = "vi_VN";
+    };
+  };
+
+  # Keyboard
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Users
   users.users.lambert = {
     isNormalUser = true;
     description = "lambert";
-    extraGroups = [ "networkmanager" "wheel" "audio" "video"];
+    extraGroups = [ "networkmanager" "wheel" "audio" "video" ];
     packages = with pkgs; [];
   };
 
-  # Enable automatic login for the user.
+  # Automatic login
   services.getty.autologinUser = "lambert";
-  programs.hyprland = {
-	enable = true;
-	xwayland.enable = true;
+
+  # Programs
+  programs = {
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+    hyprlock.enable = true;
+    firefox.enable = true;
   };
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
+  # System packages
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
     wget
     foot
     hyprland
     hyprsome
+    hyprlock
     waybar
     rofi-wayland
     wl-clipboard
     firefox
     git
-    teams-for-linux
+    p7zip
   ];
 
-  programs.firefox.enable = true;
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
+  # Services
+  # services.openssh.enable = true;  # Enable the OpenSSH daemon.
+  # networking.firewall = {  # Firewall settings
+  #   allowedTCPPorts = [ ... ];
+  #   allowedUDPPorts = [ ... ];
+  #   enable = false;  # Or disable the firewall altogether.
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  # System state version
+  system.stateVersion = "24.11";  # Did you read the comment?
 }
+
