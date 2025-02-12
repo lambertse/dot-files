6ec8304df5a7d7c5d4101a03d3d7882444ebd041
lambertse <lambert.software.engineer@gmail.com>
2025-02-12 22:07:26 +0700

    Dashboard-nvim | Custom dashboard in neovim

---
+++ dot_files/nvim/lua/plugins/dash-board.lua
@@ -0,0 +1,39 @@
+return {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup{
+        theme = 'hyper',
+        config = {
+          week_header = {
+          enable = true,
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = '  Tree',
+              group = 'DiagnosticHint',
+              action = 'Neotree toggle',
+              key = 'e',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+        },
+      }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
+
